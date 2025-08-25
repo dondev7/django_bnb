76b9e1801e822f7199d7d0fcfe37dc8e8caddad5
dondev7 <doniodns@gmail.com>
2025-08-25 17:56:55 +0700

    Submit Air BnB Challenge for chater 7

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from .models import Tweet, Like
 
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ('user', 'payload', 'created_at', 'updated_at')
-    search_fields = ('payload',)
-    list_filter = ('user', 'created_at')
+    list_display = ("user", "payload", "total_likes", "created_at", "updated_at")
+    search_fields = ("payload",)
+    list_filter = ("user", "created_at")
+
+    def total_likes(self, tweet):
+        return tweet.likes.count()
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tweet', 'created_at', 'updated_at')
-    search_fields = ('user__username', 'tweet__payload')
-    list_filter = ('user', 'tweet', 'created_at')
-
-
-
+    list_display = ("user", "tweet", "created_at", "updated_at")
+    search_fields = ("user__username", "tweet__payload")
+    list_filter = ("user", "tweet", "created_at")
