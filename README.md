2ffef47f78bc11f4e770d38f423bf0c31fc405b6
dondev7 <doniodns@gmail.com>
2025-08-27 17:00:24 +0700

    Submit Air BnB Challenge (Urls, Views and Templates)

--- README.md
+++ README.md
@@ -28,3 +28,13 @@
 
 - Use abstract classes.
 - Customize the **str** method of all classes.
+
+## Urls, Views and Templates (2025-08-27)
+
+- 오늘의 강의: 풀스택 에어비앤비 클론코딩: From #9.0 to #9.6
+- 제출기간: 익일 오전 6시까지
+
+### Mission:
+
+- Using urls.py, views.py, render() and the Django ORM, write code to show a list of all the Tweets to the user when the user visits the / page.
+- urls.py, views.py, render() 그리고 Django ORM 을 활용하여. / 페이지에 유저가 방문하였을때. 모든 Tweets 을 보여주는 리스트 코드를 작성하세요.
--- tweets/admin.py
+++ tweets/admin.py
@@ -2,55 +2,44 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
-class ElonMuskFilter(admin.SimpleListFilter):
-    """Custom filter for tweets mentioning Elon Musk"""
+class ElonFilter(admin.SimpleListFilter):
 
-    title = "Elon Musk mentions"
-    parameter_name = "elon_musk"
+    title = "Filter by like count"
+    parameter_name = "likes"
 
     def lookups(self, request, model_admin):
         return (
-            ("contains", "Contains Elon Musk"),
-            ("not_contains", "Does not contain Elon Musk"),
+            ("elon", "Tweets with Elon"),
+            ("no_elon", "Tweets without Elon"),
         )
 
-    def queryset(self, request, queryset):
-        if self.value() == "contains":
-            return queryset.filter(payload__icontains="Elon Musk")
-        elif self.value() == "not_contains":
-            return queryset.exclude(payload__icontains="Elon Musk")
-        return queryset
+    def queryset(self, request, tweets):
+        likes = self.value()
+        if likes == "elon":
+            return tweets.filter(payload__icontains="Elon Musk")
+        elif likes == "no_elon":
+            return tweets.exclude(payload__icontains="Elon Musk")
 
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = (
-        "user",
         "payload",
+        "user",
         "like_count",
-        "created_at",
     )
     search_fields = (
         "payload",
-        "user__username",  # Search by username of user foreign key
+        "user__username",
     )
     list_filter = (
+        ElonFilter,
         "created_at",
-        ElonMuskFilter,  # Custom filter for Elon Musk
-        "user",
     )
-    ordering = ("-created_at",)
-    list_per_page = 20
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = (
-        "user",
-        "tweet",
-        "created_at",
-    )
-    search_fields = ("user__username",)  # Search by username of user foreign key
+
+    search_fields = ("user__username",)
     list_filter = ("created_at",)
-    ordering = ("-created_at",)
-    list_per_page = 20
